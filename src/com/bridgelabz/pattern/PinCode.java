259c9eb7492ffc3d7dea6e0beb5a4f447bb71c2b
Kauk777 <curokky777@gmail.com>
2020-09-28 14:52:43 +0530

    [KAUSHAL]Added: UC4 adding space after 3 digit

--- src/com/bridgelabz/pattern/PinCode.java
+++ src/com/bridgelabz/pattern/PinCode.java
@@ -3,7 +3,7 @@ import java.util.regex.*;
 
 public class PinCode {
 	//Indian pin code do not start with 0
-	private static final Pattern PIN_PATTERN=Pattern.compile("^[1-9]{1}[0-9]{5}$");
+	private static final Pattern PIN_PATTERN=Pattern.compile("^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$");
 	
 	public boolean validatePin(String pin) {
 		return PIN_PATTERN.matcher(pin).matches();
@@ -11,7 +11,10 @@ public class PinCode {
 	
 	public static void main(String[] args) {
 		PinCode pinPattern=new PinCode();
+		System.out.println("Validate pin 400088: "+pinPattern.validatePin("400088"));
+		System.out.println("Validate pin A400088: "+pinPattern.validatePin("A400088"));
 		System.out.println("Validate pin 400088B: "+pinPattern.validatePin("400088B"));
+		System.out.println("Validate pin 400 088: "+pinPattern.validatePin("400 088"));
 	}
 
 }
