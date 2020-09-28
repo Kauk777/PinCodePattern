9a614d790fffe21d78095a6e4bc34e0503682580
Kauk777 <curokky777@gmail.com>
2020-09-28 14:35:28 +0530

    [KAUSHAL]Added: UC1 simple pin validation

---
+++ src/com/bridgelabz/pattern/PinCode.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.pattern;
+import java.util.regex.*;
+
+public class PinCode {
+	//Indian pin code do not start with 0
+	private static final Pattern PIN_PATTERN=Pattern.compile("^[1-9]{1}[0-9]{5}$");
+	
+	public boolean validatePin(String pin) {
+		return PIN_PATTERN.matcher(pin).matches();
+	}
+	
+	public static void main(String[] args) {
+		PinCode pinPattern=new PinCode();
+		System.out.println("Validate pin 400088: "+pinPattern.validatePin("400088"));
+	}
+
+}
